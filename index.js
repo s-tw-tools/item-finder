ecb7fd40e93bd9c9b327615092b2687fb6c527ab
s-tw-tools <116835989+s-tw-tools@users.noreply.github.com>
2022-10-27 21:54:38 +0200

    feature/item-finder

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-
+let DataStorage = (function(){
+    function DataStore() {
+        this.data = undefined;
+        let requestOptions = {
+            method: 'GET',
+            redirect: 'follow'
+        };
+        
+        this.sortItemsBySkill = function(skillName){
+            return this.data.sort(function(a,b){
+                let valueOfA = a.bonus.item.find(function(bonusItem){
+                    if(bonusItem.bonus != undefined)
+                        return(bonusItem.bonus.name == skillName)
+                })
+                let valueOfB = b.bonus.item.find(function(bonusItem){
+                    if(bonusItem.bonus != undefined)
+                        return(bonusItem.bonus.name == skillName)
+                })
+                return ((valueOfA == undefined) ? 0 : valueOfA.bonus.value) - ((valueOfB == undefined) ? 0 : valueOfB.bonus.value); 
+            }).reverse()
+        }
+  
+        fetch("https://s-tw-tools.github.io/item-finder/items_en_DK.json", requestOptions)
+            .then(response => response.json())
+            .then(result => {this.data = result})
+            .catch(error => console.log('error', error));
+    }
+  
+    let instance;
+  
+    return {
+      getDataStorage: function(){
+        if (!instance) {
+          instance = new DataStore();
+          delete instance.constructor;
+        }
+        return instance;
+      }
+    };
+})();
+DataStorage.getDataStorage();
