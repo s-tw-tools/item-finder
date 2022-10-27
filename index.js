9b567676c137be4fd10a6fb42871c48ab8169cee
s-tw-tools <s-tw-tools@users.noreply.github.com>
2022-10-27 19:54:56 +0000

    deploy: 9f01497e821126edea5785fb3cbf41a949880b5a

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+let DataStorage = (function(){
+    let data; 
 
+    function loadData() {
+        var requestOptions = {
+            method: 'GET',
+            redirect: 'follow'
+        };
+          
+        fetch("https://s-tw-tools.github.io/item-finder/items_en_DK.json", requestOptions)
+            .then(response => response.json())
+            .then(result => {data = result})
+            .catch(error => console.log('error', error));
+    }
+    
+    return {
+      getData: function(){
+        if (!data) {
+          data = new loadData();
+        }
+        while(!data)
+        return data;
+      }
+    };
+})();
